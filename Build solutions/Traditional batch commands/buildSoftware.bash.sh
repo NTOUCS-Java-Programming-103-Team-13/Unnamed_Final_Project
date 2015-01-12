a6485a13f8d2dc87e670338c638988287f6b9120
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2015-01-12 20:33:11 +0800

    更新程式碼以啟用 GUI
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- Build solutions/Traditional batch commands/buildSoftware.bash.sh
+++ Build solutions/Traditional batch commands/buildSoftware.bash.sh
@@ -8,11 +8,13 @@
 # 	在命令列介面中切換當前工作目錄(current working directory)到本軟體建構解決方案根目錄下，然後執行下列命令：
 # 	$ bash buildSoftware.bash.sh
 
+global_project_root_directory=../..
+
 # 啟用 bash 殼程式直譯器的偵錯功能，方便看到腳本程式呼叫了什麼命令
 set -x
 
 # 呼叫 Java 編譯器(javac)將 Java 來源程式碼編譯為 Java 虛擬機器能載入執行的 bytecode
-javac -d "Built software/" "../../Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/MainProgram.java"
+javac -classpath "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper" -d "Built software/" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/MainProgram.java" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java"
 
 # 回傳 0 結束狀態代碼， 0 代表正常結束程式
 exit 0
