86a66195a6de3e5ed89eeb9e785f8b70a222f1c0
林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>
2015-01-15 05:48:32 +0800

    Traditional batch commands/buildSoftware.bash.sh - 強制指定來源程式碼字元編碼為 UTF-8，修正 Windows 建構失敗問題
    Signed-off-by: 林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>

--- Build solutions/Traditional batch commands/buildSoftware.bash.sh
+++ Build solutions/Traditional batch commands/buildSoftware.bash.sh
@@ -15,7 +15,7 @@ set -x
 
 # 呼叫 Java 編譯器(javac)將 Java 來源程式碼編譯為 Java 虛擬機器能載入執行的 bytecode
 # FIXME: 用於 -classpath 參數的路徑分隔符（半形冒號(:)）目前僅相容類 Unix 作業系統，Microsoft Windows 上需手動改為半形分號(;)）
-javac -classpath "${global_project_root_directory}/Source code":"${global_project_root_directory}/Libraries/org.json.jar" -d "Built software/" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/MainProgram.java" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/YoutubeDlParser.java"
+javac -encoding UTF-8 -classpath "${global_project_root_directory}/Source code":"${global_project_root_directory}/Libraries/org.json.jar" -d "Built software/" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/MainProgram.java" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java" "${global_project_root_directory}/Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/YoutubeDlParser.java"
 
 # 將 Java archive 複製到建構好的軟體目錄
 cp --force --recursive "${global_project_root_directory}/Libraries" "Built software/"
