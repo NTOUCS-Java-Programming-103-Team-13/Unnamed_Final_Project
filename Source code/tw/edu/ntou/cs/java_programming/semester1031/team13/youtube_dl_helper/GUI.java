6b43c86d51ac485bf6c35a59fab9058bb281afd2
林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>
2015-01-15 06:23:58 +0800

    GUI - 移除用不到的片斷
    Signed-off-by: 林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>

--- Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java
+++ Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java
@@ -15,9 +15,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import javax.swing.*;
 
@@ -136,7 +133,7 @@ public class GUI extends JFrame implements ActionListener{
 		int result;
 		if(event.getActionCommand().equals("選擇‧‧‧‧‧")){
 			result = savePathChooser.showSaveDialog(frame);
-			file = null;
+
 			if(result==JFileChooser.APPROVE_OPTION){
 				file = savePathChooser.getSelectedFile();
 				媒體保存目錄輸入框.setText(savePathChooser.getSelectedFile().getAbsolutePath());
