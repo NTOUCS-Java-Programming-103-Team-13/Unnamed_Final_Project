2e0f86dd453dcf4be74f182f1277412f6394fa4a
林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>
2015-01-14 12:19:22 +0800

    GUI - 重構 - 改善元件名稱可讀性
    Signed-off-by: 林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>

--- Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java
+++ Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java
@@ -31,20 +31,20 @@ public class GUI extends JFrame implements ActionListener{
 	/////////////////頁籤///////////////////////////
 	private JTabbedPane view = new JTabbedPane (JTabbedPane.TOP);
 	/////////////////主畫面元件///////////////////////////
-	private	JLabel 媒體網址 = new JLabel("媒體網址 :");
-	private	JTextField 網址 = new JTextField(100);
-	private	JLabel 網址分析結果 = new JLabel("網址分析結果:");
-	private	JLabel 選擇下載格式 = new JLabel("選擇下載格式:");
-	private	JTextField 下載格式 = new JTextField(100);
-	private	JLabel 選擇字幕語言 = new JLabel("選擇字幕語言:");
-	private	JTextField 字幕語言 = new JTextField(100);
-	private	JLabel 保存目錄 = new JLabel("保存目錄:");
-	private JLabel 呼叫命令 = new JLabel("youtube-dl 呼叫命令:");
-	private	JTextField 命令 = new JTextField(100);
-	private	JScrollPane 結果 = new JScrollPane();
-	private JButton 選擇 = new JButton("選擇‧‧‧‧‧");
-	private JButton 執行 = new JButton("執行");
-	private JLabel 下載結果 = new JLabel("下載結果:");
+	private	JLabel 媒體網址輸入框標籤 = new JLabel("媒體網址 :");
+	private	JTextField 媒體網址輸入框 = new JTextField(100);
+	private	JLabel 網址分析結果標籤 = new JLabel("網址分析結果:");
+	private	JLabel 媒體支援格式清單標籤 = new JLabel("選擇下載格式:");
+	private	JTextField 媒體支援格式清單 = new JTextField(100);
+	private	JLabel 媒體支援字幕語言清單標籤 = new JLabel("選擇字幕語言:");
+	private	JTextField 媒體支援字幕語言清單 = new JTextField(100);
+	private	JLabel 選擇媒體保存目錄標籤 = new JLabel("保存目錄:");
+	private JLabel 呼叫命令標籤 = new JLabel("youtube-dl 呼叫命令:");
+	private	JTextField 呼叫命令輸入框 = new JTextField(100);
+	private	JScrollPane 命令執行結果 = new JScrollPane();
+	private JButton 選擇保存目錄按鈕 = new JButton("選擇‧‧‧‧‧");
+	private JButton 執行下載命令按鈕 = new JButton("執行");
+	private JLabel 命令執行結果標籤 = new JLabel("下載結果:");
 	////////////////////////////檔案儲存///////////////////
 	private JFileChooser fileChooser = null;
 	JTextArea textarea = null;
@@ -55,55 +55,55 @@ public class GUI extends JFrame implements ActionListener{
 			JPanel jpCenter2 = new JPanel();//創立介紹容器
 			jpCenter.setLayout(null);
 			/////////////////大小區//////////////////////////
-			呼叫命令.setSize(new Dimension(200,30));
-			命令.setSize(new Dimension(600,30));
-			保存目錄.setSize(new Dimension(800,30));
-			執行.setSize(new Dimension(75,30));
-			選擇.setSize(new Dimension(125,30));
-			結果.setSize(new Dimension(865,200));
-			下載結果.setSize(new Dimension(200,30));
-			下載格式.setSize(new Dimension(200,150));
-			字幕語言.setSize(new Dimension(200,150));
-			選擇下載格式.setSize(new Dimension(200,30));
-			選擇字幕語言.setSize(new Dimension(200,30));
-			網址分析結果.setSize(new Dimension(200,30));
-			媒體網址.setSize(new Dimension(200,30));
-			網址.setSize(new Dimension(800,30));
+			呼叫命令標籤.setSize(new Dimension(200,30));
+			呼叫命令輸入框.setSize(new Dimension(600,30));
+			選擇媒體保存目錄標籤.setSize(new Dimension(800,30));
+			執行下載命令按鈕.setSize(new Dimension(75,30));
+			選擇保存目錄按鈕.setSize(new Dimension(125,30));
+			命令執行結果.setSize(new Dimension(865,200));
+			命令執行結果標籤.setSize(new Dimension(200,30));
+			媒體支援格式清單.setSize(new Dimension(200,150));
+			媒體支援字幕語言清單.setSize(new Dimension(200,150));
+			媒體支援格式清單標籤.setSize(new Dimension(200,30));
+			媒體支援字幕語言清單標籤.setSize(new Dimension(200,30));
+			網址分析結果標籤.setSize(new Dimension(200,30));
+			媒體網址輸入框標籤.setSize(new Dimension(200,30));
+			媒體網址輸入框.setSize(new Dimension(800,30));
 			//////////////////位置區/////////////////////////
-			保存目錄.setLocation(30,350);
-			呼叫命令.setLocation(30,400);
-			命令.setLocation(150,400);
-			選擇.setLocation(770, 350);
-			執行.setLocation(820, 400);
-			結果.setLocation(30, 450);
-			下載結果.setLocation(30,650);
-			下載格式.setLocation(30,180);
-			字幕語言.setLocation(240,180);
-			選擇字幕語言.setLocation(240,150);
-			選擇下載格式.setLocation(30,150);
-			網址分析結果.setLocation(30,120);
-			媒體網址.setLocation(30, 80);
-			網址.setLocation(95,80);
+			選擇媒體保存目錄標籤.setLocation(30,350);
+			呼叫命令標籤.setLocation(30,400);
+			呼叫命令輸入框.setLocation(150,400);
+			選擇保存目錄按鈕.setLocation(770, 350);
+			執行下載命令按鈕.setLocation(820, 400);
+			命令執行結果.setLocation(30, 450);
+			命令執行結果標籤.setLocation(30,650);
+			媒體支援格式清單.setLocation(30,180);
+			媒體支援字幕語言清單.setLocation(240,180);
+			媒體支援字幕語言清單標籤.setLocation(240,150);
+			媒體支援格式清單標籤.setLocation(30,150);
+			網址分析結果標籤.setLocation(30,120);
+			媒體網址輸入框標籤.setLocation(30, 0);
+			媒體網址輸入框.setLocation(95,0);
 			/////////////////加入元件//////////////////////
-			jpCenter.add(媒體網址); //將元件加入JPanel子容器
-			jpCenter.add(網址);
-			jpCenter.add(網址分析結果);
-			jpCenter.add(選擇下載格式);
-			jpCenter.add(下載格式);
-			jpCenter.add(選擇字幕語言);
-			jpCenter.add(字幕語言);
-			jpCenter.add(保存目錄);
-			jpCenter.add(呼叫命令);
-			jpCenter.add(選擇);
-			jpCenter.add(執行);
-			jpCenter.add(下載結果);
-			jpCenter.add(命令);
-			jpCenter.add(結果);
+			jpCenter.add(媒體網址輸入框標籤); //將元件加入JPanel子容器
+			jpCenter.add(媒體網址輸入框);
+			jpCenter.add(網址分析結果標籤);
+			jpCenter.add(媒體支援格式清單標籤);
+			jpCenter.add(媒體支援格式清單);
+			jpCenter.add(媒體支援字幕語言清單標籤);
+			jpCenter.add(媒體支援字幕語言清單);
+			jpCenter.add(選擇媒體保存目錄標籤);
+			jpCenter.add(呼叫命令標籤);
+			jpCenter.add(選擇保存目錄按鈕);
+			jpCenter.add(執行下載命令按鈕);
+			jpCenter.add(命令執行結果標籤);
+			jpCenter.add(呼叫命令輸入框);
+			jpCenter.add(命令執行結果);
 			///////////////////////////加入頁籤///////////////////////////
 			view.addTab("下載媒體",jpCenter);
 			view.addTab("關於本軟體",jpCenter2);
 			///////////////////////////存檔功能//////////////////////////
-			選擇.addActionListener((ActionListener) this);
+			選擇保存目錄按鈕.addActionListener((ActionListener) this);
 			fileChooser = new JFileChooser();
 			//////////////////////////////////////////////////////////
 			Container cp = getContentPane(); //取得內容面版
@@ -121,15 +121,15 @@ public class GUI extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent event){//選擇檔案存檔
 		File file=null;
 		int result;
-		if(event.getActionCommand().equals("選擇‧‧‧‧‧")){
+		if(event.getActionCommand().equals("選擇保存目錄按鈕‧‧‧‧‧")){
 			result = fileChooser.showSaveDialog(frame);
 			file = null;
 			if(result==JFileChooser.APPROVE_OPTION){
 				file = fileChooser.getSelectedFile();
-				保存目錄.setText("保存目錄:"+fileChooser.getSelectedFile());
+				選擇媒體保存目錄標籤.setText("選擇媒體保存目錄標籤:"+fileChooser.getSelectedFile());
 			}
 			else if(result==JFileChooser.CANCEL_OPTION){
-				保存目錄.setText("保存目錄:");
+				選擇媒體保存目錄標籤.setText("選擇媒體保存目錄標籤:");
 			}
 			
 			FileOutputStream fileOutStream = null;
@@ -138,7 +138,7 @@ public class GUI extends JFrame implements ActionListener{
 				try{
 					fileOutStream = new FileOutputStream(file);
 				}catch(FileNotFoundException fe){
-					保存目錄.setText("找不到檔案");
+					選擇媒體保存目錄標籤.setText("找不到檔案");
 					return;
 				}
 				
@@ -147,7 +147,7 @@ public class GUI extends JFrame implements ActionListener{
 			try{
 				fileOutStream.write(content.getBytes());
 			}catch(IOException ioe){
-				保存目錄.setText("寫入檔案錯誤");
+				選擇媒體保存目錄標籤.setText("寫入檔案錯誤");
 			}
 			finally{
 				try{
