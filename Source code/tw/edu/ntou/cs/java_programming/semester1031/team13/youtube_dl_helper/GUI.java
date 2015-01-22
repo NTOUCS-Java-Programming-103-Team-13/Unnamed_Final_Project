8d8609060b4a031510673f15eec00d7a4918046c
林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>
2015-01-22 23:44:57 +0800

    GUI - 移除無用程式片段
    Signed-off-by: 林博仁(09957010) <Henry.Lin.Taiwan@gmail.com>

--- Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java
+++ Source code/tw/edu/ntou/cs/java_programming/semester1031/team13/youtube_dl_helper/GUI.java
@@ -31,9 +31,6 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 /**
  * @brief GUI class
  */
